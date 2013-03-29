cae68992e013d2770fdeff496b331125934d0529
xyc <xyc@bnc.com.cn>
2013-03-29 14:11:14 +0800

    add sem.c and test_sem.c.

---
+++ src/sem.c
@@ -0,0 +1,121 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/sem.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <stdlib.h>
+#include <errno.h>
+
+union semun{
+    int val;
+    struct semid_ds *buf;
+    unsigned short int *array;
+    struct seminfo *__buf;
+};
+
+/* create semaphore */
+int create_sem(int key_id, int count)
+{
+    key_t key;
+    key= ftok(".", key_id);
+
+    int semid;
+    semid= semget(key, count, 0666| IPC_CREAT);
+    if(-1== semid){
+        printf("create sem error: %s, semid= %d\n", strerror(errno), semid);
+    }
+
+    return semid;
+}
+/* delete semaphore */
+int free_sem(int semid)
+{
+    union semun ignored_arg;
+    return semctl(semid, 0, IPC_RMID, ignored_arg);
+}
+/* set semaphore value */
+int set_sem_value(int semid, int index, int value)
+{
+    union semun arg;
+    arg.val= value;
+    int ret;
+    ret= semctl(semid, index, SETVAL, arg);
+    if(-1== ret){
+        printf("set_sem_value error: %s\n",strerror(errno));
+    }
+
+    return ret;
+}
+/* get semaphore value */
+int get_sem_value(int semid, int index)
+{
+    int ret= semctl(semid, index, GETVAL, 0);
+    if(-1== ret){
+        printf("get_sem_value error: %s\n",strerror(errno));
+    }
+
+    return ret;
+}
+/* block when semid[index] is negative and ms has not timeout. */
+int try_lock_sem(int semid, int index, int ms)
+{
+    int ret;
+    struct sembuf op;
+    op.sem_num= index;
+    op.sem_op= -1;
+
+    if(ms< 1){
+        op.sem_flg= IPC_NOWAIT;
+        ret= semop(semid, &op, 1);
+        if(-1== ret){
+            printf("try_lock_sem error: %s\n",strerror(errno));
+        }
+    }
+    else
+    {
+        /* to avoid unwanted exited */
+        op.sem_flg= SEM_UNDO;
+        struct timespec timeout;
+        timeout.tv_sec= ms/ 1000;
+        timeout.tv_nsec= (ms% 1000L)* 1000000L;
+        ret= semtimedop(semid, &op, 1, &timeout);
+        if(-1== ret){
+            printf("try_lock_sem error: %s\n",strerror(errno));
+        }
+    }
+
+//    if(-1== ret){
+//        if(errno== EAGAIN) return EAGAIN;
+//    }
+    return ret;
+}
+/* block when sem[index] is negative, until it become plus */
+int lock_sem(int semid, int index)
+{
+    struct sembuf op;
+    op.sem_num= index;
+    op.sem_op= -1;
+    op.sem_flg= SEM_UNDO;
+    int ret;
+    ret= semop(semid, &op, 1);
+    if(-1== ret){
+        printf("pid[%d] lock_sem error: %s\n", getpid(), strerror(errno));
+    }
+    return ret;
+}
+/* unlock semaphore */
+int unlock_sem(int semid, int index)
+{
+    struct sembuf op;
+    op.sem_num= index;
+    op.sem_op= 1;
+    op.sem_flg= SEM_UNDO;
+
+    int ret;
+    ret= semop(semid, &op, 1);
+    if(-1== ret){
+        printf("pid[%d] unlock_sem error: %s, semid=%d index=%d\n", getpid(), strerror(errno), semid, index);
+    }
+
+    return ret;
+}
---
+++ src/test_sem.c
@@ -0,0 +1,65 @@
+#include "sem.h"
+#include "stdio.h"
+
+#define SEM_ID 11
+
+int main()
+{
+    pid_t pid;
+    int semid;
+    semid= create_sem(SEM_ID, 2);
+    set_sem_value(semid, 0, 1);
+    set_sem_value(semid, 1, 0);
+
+    long times= 0;
+    long times1= 0;
+
+    pid= fork();
+    if(pid== 0)
+    {
+        int ret;
+
+        srand((int)time(0));
+
+        while(1)
+        {
+            ret= lock_sem(semid, 1);
+            if(-1== ret){
+                int t= get_sem_value(semid, 1);
+                printf("pid[%d] current semid index[%d] value is %d\n", getpid(), 1, t);
+            }
+            printf("pid[%d] get consumer signal times= %d\n",getpid(), times1++);
+            if(times1> 30000) times1= 1;
+
+            ret= set_sem_value(semid, 0, 1);
+            if(-1== ret){
+                int t= get_sem_value(semid, 0);
+                printf("pid[%d] current semid index[%d] value is %d\n", getpid(), 0, t);
+            }
+        }
+        return 0;
+    }
+
+    while(1)
+    {
+        int ret;
+
+        srand((int)time(0)/3);
+
+        ret= lock_sem(semid, 0);
+        if(-1== ret){
+            int t= get_sem_value(semid, 0);
+            printf("pid[%d] current semid index[%d] value is %d\n", getpid(), 0, t);
+        }
+        printf("pid[%d] get producter signal times= %d\n", getpid(), times++);
+        if(times> 30000) times= 1;
+        ret= set_sem_value(semid, 1, 1);
+        if(-1== ret){
+            int t= get_sem_value(semid, 1);
+            printf("pid[%d] current semid index[%d] value is %d\n", getpid(), 1, t);
+        }
+    }
+
+    free_sem(semid);
+    return 0;
+}
